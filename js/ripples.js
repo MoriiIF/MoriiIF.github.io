8f4d61c5b28ba79fbeff406314450388bac1e85b
MoriiIF <MoriiIF_221B@yeah.net>
2024-05-20 22:43:24 +0800

    Site updated: 2024-05-20 22:43:22

--- js/ripples.js
+++ js/ripples.js
@@ -1,11 +1,15 @@
-$(document).ready(function () {
-  try {
-    $("#page-header").ripples({
-      resolution: 512,
-      dropRadius: 10, //px
-      perturbance: 0.04,
+<script src="https://rmt.dogedoge.com/fetch/~/source/jsdelivr/npm/jquery@latest/dist/jquery.min.js"></script>
+<script defer src="https://cdn.jsdelivr.net/gh/sirxemic/jquery.ripples/dist/jquery.ripples.js"></script>
+<script>
+    $(document).ready(function () {
+        try {
+            $("#page-header").ripples({
+            resolution: 512,
+            dropRadius: 10, //px
+            perturbance: 0.04,
+            });
+        } catch (e) {
+            $(".error").show().text(e);
+        }
     });
-  } catch (e) {
-    $(".error").show().text(e);
-  }
-});
+</script>
